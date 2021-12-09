2527c0032a6c4671f9db221f4cbf9c2d3f09b6a4
Frerich Raabe <frerich.raabe@betterdoc.de>
2021-12-09 23:05:19 +0100

    Further simplifications & Doctests for Day 8 Part 2

--- day8/lib/day8.ex
+++ day8/lib/day8.ex
@@ -47,10 +47,10 @@ defmodule Day8 do
     File.read!(input_file)
     |> parse()
     |> Enum.map(fn {patterns, digits} ->
-      assignment = pick(patterns)
+      assignment = deduce_wire_assignment(patterns)
 
       digits
-      |> Enum.map(& pattern_to_digit(&1, assignment))
+      |> Enum.map(&pattern_to_digit(&1, assignment))
       |> Integer.undigits()
     end)
     |> Enum.sum()
@@ -90,48 +90,66 @@ defmodule Day8 do
     {patterns, digits}
   end
 
-  def candidates(patterns) do
-    Enum.map(0..9, fn digit ->
-      num_segments =
-        Enum.find_value(@digits, fn {segments, d} -> if d === digit, do: Enum.count(segments) end)
+  @doc ~S"""
+  Deduced the wire-to-segment assignment given a list of signal patterns.
 
-      Enum.filter(patterns, fn wires -> Enum.count(wires) == num_segments end)
-    end)
-  end
+  ## Examples
 
-  def pick(patterns) do
-    candidates = candidates(patterns)
-
+      iex> Day8.deduce_wire_assignment(['abcdefg', 'bcdef', 'acdfg', 'abcdf', 'abd', 'abcdef', 'bcdefg', 'abef', 'abcdeg', 'ab'])
+      %{97 => 99, 98 => 102, 99 => 103, 100 => 97, 101 => 98, 102 => 100, 103 => 101}
+  """
+  def deduce_wire_assignment(patterns) do
     [assignment] =
       for {c, rest} <- select('abcdefg'),
           {f, rest} <- select(rest),
-          Enum.any?(Enum.at(candidates, 1), fn wires -> [c, f] -- wires == [] end),
+          Enum.sort([c, f]) in patterns,
           {a, rest} <- select(rest),
-          Enum.any?(Enum.at(candidates, 7), fn wires -> [a, c, f] -- wires == [] end),
+          Enum.sort([a, c, f]) in patterns,
           {b, rest} <- select(rest),
           {d, rest} <- select(rest),
-          Enum.any?(Enum.at(candidates, 4), fn wires -> [b, c, d, f] -- wires == [] end),
+          Enum.sort([b, c, d, f]) in patterns,
           {g, rest} <- select(rest),
-          Enum.any?(Enum.at(candidates, 3), fn wires -> [a, c, d, f, g] -- wires == [] end),
-          Enum.any?(Enum.at(candidates, 5), fn wires -> [a, b, d, f, g] -- wires == [] end),
-          Enum.any?(Enum.at(candidates, 9), fn wires -> [a, b, c, d, f, g] -- wires == [] end),
+          Enum.sort([a, c, d, f, g]) in patterns,
+          Enum.sort([a, b, d, f, g]) in patterns,
+          Enum.sort([a, b, c, d, f, g]) in patterns,
           {e, []} <- select(rest),
-          Enum.any?(Enum.at(candidates, 2), fn wires -> [a, c, d, e, g] -- wires == [] end),
-          Enum.any?(Enum.at(candidates, 6), fn wires -> [a, b, d, e, f, g] -- wires == [] end),
-          Enum.any?(Enum.at(candidates, 8), fn wires -> [a, b, c, d, e, f, g] -- wires == [] end),
-          Enum.any?(Enum.at(candidates, 0), fn wires -> [a, b, c, e, f, g] -- wires == [] end) do
+          Enum.sort([a, c, d, e, g]) in patterns,
+          Enum.sort([a, b, d, e, f, g]) in patterns,
+          Enum.sort([a, b, c, d, e, f, g]) in patterns,
+          Enum.sort([a, b, c, e, f, g]) in patterns do
         %{a => ?a, b => ?b, c => ?c, d => ?d, e => ?e, f => ?f, g => ?g}
       end
 
     assignment
   end
 
+  @doc ~S"""
+  Translates a signal pattern (identified by the wires giving a signal) and an
+  assignment table which tells which segments to light for each wire into a
+  digit.
+
+  ## Examples
+
+      iex> Day8.pattern_to_digit('ab', %{?a => ?a, ?b => ?b, ?c => ?c, ?d => ?d, ?e => ?e, ?f => ?f, ?g => ?g})
+      nil
+
+      iex> Day8.pattern_to_digit('cf', %{?a => ?a, ?b => ?b, ?c => ?c, ?d => ?d, ?e => ?e, ?f => ?f, ?g => ?g})
+      1
+  """
   def pattern_to_digit(pattern, assignment) do
     segments = Enum.map(pattern, fn wire -> Map.get(assignment, wire) end)
 
     Map.get(@digits, Enum.sort(segments))
   end
 
+  @doc ~S"""
+  Returns all ways to draw one element from the given list.
+
+  ## Examples
+
+      iex> Day8.select([1,2,3,4])
+      [{1, [2,3,4]}, {2, [1,3,4]}, {3, [1,2,4]}, {4, [1,2,3]}]
+  """
   def select(list) do
     Enum.map(0..(Enum.count(list) - 1), &List.pop_at(list, &1))
   end
